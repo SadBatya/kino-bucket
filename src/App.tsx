2cfc021afcf94c5a6d5bffaf799607186df0e60c
sadbatya <sadbatya@mail.ru>
2024-09-19 17:37:51 -0400

    add styles in component navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/App.scss';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 function App() {
   return (
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,4 @@ export { AuthorizationPage } from './Authorization/AuthorizationPage'
 export { FavouritesPage } from './Favourites/FavouritesPage';
 export { FilmListPage } from './FilmList/FilmListPage'
 export {LoginPage} from './Login/LoginPage'
-
+export { ErrorPage } from './ErrorPage/ErrorPage'

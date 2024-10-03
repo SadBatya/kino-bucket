0afcd014632c6b9f67936406898bcdf4c0822e25
sadbatya <sadbatya@mail.ru>
2024-10-02 20:06:06 -0400

    add: добавил error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,18 @@
+import style from "./ErrorPage.module.scss";
+import { Link } from "react-router-dom";
+import cat from "../../assets/gifs/cat.webp";
+
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className={style.error}>
+      <div>
+        <h1 className={style.error__text}>Упс! Что-то пошло не так :c</h1>
+        <h2>Уже работаем над этим :)</h2>
+      </div>
+      <img className={style.error__gif} src={cat} alt="cat" />
+      <Link className={style.error__btn} to="/">
+        Вернуться на главную
+      </Link>
+    </div>
+  );
+};

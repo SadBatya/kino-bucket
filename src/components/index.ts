36acc16f379fd3a1511ec6530927f01edd594e80
sadbatya <sadbatya@mail.ru>
2024-10-06 14:57:16 -0400

    add: добавил футер

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { Navbar } from "./Navbar/Navbar";
 export { FilmCard } from "./FilmCard/FilmCard";
 export { SubmitButton } from "./SubmitButton/SubmitButton";
-
+export { Footer } from './Footer/Footer'
 export { Loader } from "./Loader/Loader";
